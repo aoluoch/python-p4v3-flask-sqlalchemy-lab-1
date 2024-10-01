beb8f1c08174ff4ceee1be6bcf91a4c320d57632
Amos Oluoch <aoluoch271@gmail.com>
2024-10-01 15:40:45 +0300

    sql

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +21,46 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+# View to get earthquake by ID
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    '''Has a resource available at "/earthquakes/<id>".'''
+    earthquake = Earthquake.query.get(id)
+
+    if earthquake is None:
+        # Return error message if no earthquake is found
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+
+    # Return earthquake details in JSON format
+    return jsonify({
+        'id': earthquake.id,
+        'location': earthquake.location,
+        'magnitude': earthquake.magnitude,
+        'year': earthquake.year
+    }), 200
+
+
+# View to get earthquakes matching a minimum magnitude
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    '''Fetches earthquakes with a magnitude greater than or equal to the provided value.'''
+    
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    # Prepare a list of earthquake details
+    quakes_list = [{
+        'id': quake.id,
+        'location': quake.location,
+        'magnitude': quake.magnitude,
+        'year': quake.year
+    } for quake in earthquakes]
+
+    # Return count of results and the earthquake data
+    return jsonify({
+        'count': len(quakes_list),
+        'quakes': quakes_list
+    }), 200
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,15 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    # Define columns
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    # Representation method
+    def __repr__(self):
+        return f"<Earthquake(id={self.id}, magnitude={self.magnitude}, location={self.location}, year={self.year})>"
